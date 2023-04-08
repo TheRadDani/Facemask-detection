1832404f5ab1f9a7599d7e6d0d33d6a9df3193de
TheRadDani <ldanielfch@gmail.com>
2023-04-07 22:12:48 -0600

    everything to update

--- train_mask_detector.py
+++ train_mask_detector.py
@@ -39,15 +39,15 @@ labels = []
 for category in CATEGORIES:
     path = os.path.join(DIRECTORY, category)
     for img in os.listdir(path):
-    	img_path = os.path.join(path, img)
-    	image = load_img(img_path, target_size=(224, 224))
-    	image = img_to_array(image)
-    	image = preprocess_input(image)
+		img_path = os.path.join(path, img)
+		image = load_img(img_path, target_size=(224, 224))
+		image = img_to_array(image)
+		image = preprocess_input(image)
 
-    	data.append(image)
-    	labels.append(category)
+		data.append(image)
+		labels.append(category)
 
-# perform one-hot encoding on the labels
+	# perform one-hot encoding on the labels
 lb = LabelBinarizer()
 labels = lb.fit_transform(labels)
 labels = to_categorical(labels)
